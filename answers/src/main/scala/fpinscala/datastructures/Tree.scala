1a2f06c03b4cd9a9dbdf0d4d38f59df6b2585369
Glenn Murray <glenn.murray@gmail.com>
2017-10-08 17:56:23 -0600

    Finished 3.29, last of the Chp. 3 exercises.

--- answers/src/main/scala/fpinscala/datastructures/Tree.scala
+++ answers/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -38,9 +38,12 @@ object Tree {
   }
 
   /* 
-  Like `foldRight` for lists, `fold` receives a "handler" for each of the data constructors of the type, and recursively
-  accumulates some value using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use
-  this function to implement just about any recursive function that would otherwise be defined by pattern matching.
+  Like `foldRight` for lists, `fold` receives a "handler" for each of the 
+  data constructors of the type, and recursively
+  accumulates some value using these handlers. As with `foldRight`, 
+  `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use
+  this function to implement just about any recursive function that would
+  otherwise be defined by pattern matching.
   */
   def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
     case Leaf(a) => f(a)
@@ -57,7 +60,9 @@ object Tree {
     fold(t)(a => 0)((d1,d2) => 1 + (d1 max d2))
   
   /*
-  Note the type annotation required on the expression `Leaf(f(a))`. Without this annotation, we get an error like this: 
+   Below:
+   Note the Tree[B] type annotation required on the expression
+   `Leaf(f(a))`. Without this annotation, we get an error like this: 
   
   type mismatch;
     found   : fpinscala.datastructures.Branch[B]
@@ -65,12 +70,15 @@ object Tree {
        fold(t)(a => Leaf(f(a)))(Branch(_,_))
                                       ^  
   
-  This error is an unfortunate consequence of Scala using subtyping to encode algebraic data types. Without the
-  annotation, the result type of the fold gets inferred as `Leaf[B]` and it is then expected that the second argument
-  to `fold` will return `Leaf[B]`, which it doesn't (it returns `Branch[B]`). Really, we'd prefer Scala to
-  infer `Tree[B]` as the result type in both cases. When working with algebraic data types in Scala, it's somewhat
-  common to define helper functions that simply call the corresponding data constructors but give the less specific
-  result type:
+   This error is an unfortunate consequence of Scala using subtyping to
+   encode algebraic data types. Without the annotation, the result type of
+   the fold gets inferred as `Leaf[B]` and it is then expected that the
+   second argument to `fold` will return `Leaf[B]`, which it doesn't
+   (it returns `Branch[B]`). Really, we'd prefer Scala to
+   infer `Tree[B]` as the result type in both cases.  When working with
+   algebraic data types in Scala, it's somewhat common to define 
+   helper functions that simply call the corresponding data constructors
+   but give the less specific result type:
     
     def leaf[A](a: A): Tree[A] = Leaf(a)
     def branch[A](l: Tree[A], r: Tree[A]): Tree[A] = Branch(l, r)
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -27,7 +27,49 @@ object Tree {
   def depth[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 0 // Why not count the leaf?  Because we count the root.
     case Branch(l, r) => 1 + (depth(l) max depth(r))
-  } 
+  }
+
+  // Ex 3.28.  Write a function map, analogous to "map" List,
+  // that modifies each element in a tree with a given function.
+  //def map[A,B](l: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  // Ex. 3.29.  Generalize size, maximum, depth, and map, writing a new
+  // function fold that abstracts over their similarities. Reimplement
+  // them in terms of this more general function.
+  //
+  // One would think fold could be implemented like List,
+  //   def fold[A,B](t: Tree[A], z: B)(f: (B, A) => B): B = ???
+  // but they have something else in mind: a "handler" for each of the
+  // data constructors of the type.  So,
+  //   def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g( fold(l)(f)(g), fold(r)(f)(g) )
+  }
+
+//  def foldSize[A](t: Tree[A]): Int = fold(t)(a => 1)((b1, b2) => 1 + b1 + b2)
+  // Better:
+  def foldSize[A](t: Tree[A]): Int = fold(t)(a => 1)(1 + _ + _)
+
+  def foldMaximum(t: Tree[Int]): Int = fold(t)(i => i)(_ max _)
+
+  def foldDepth[A](t: Tree[A]): Int = fold(t)(i => 0)(
+    (b1, b2) => 1 + (b1 max b2))
+  // This won't compile (!--compare to foldSize) because the compiler needs
+  // to know for max that the underscores are Ints.
+  //def foldDepth[A](t: Tree[A]): Int = fold(t)(i => 0)((_ max _) + 1)
+
+  def foldMap[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    // This Leaf needs a Tree type annotation because otherwise the compiler
+    // infers the type of B in the fold def to be Leaf[B], which breaks g.
+    //fold(t)(a => Leaf(f(a))( (b1, b2) => Branch(b1, b2) )
+    fold(t)(a => Leaf(f(a)): Tree[B])( (b1, b2) => Branch(b1, b2) )
+
+
 
 }
 
@@ -57,6 +99,7 @@ object TreeExercises {
     // Ex. 3.25 Write a function size that counts the number
     // of nodes (leaves and branches) in a tree.
     assert(Tree.size(sT) == 7)
+    assert(Tree.size(iT) == 9)
 
     // Ex. 3.26 Write maximum() for a Tree[Int].
     assert(Tree.maximum(iT) == 5)
@@ -65,8 +108,27 @@ object TreeExercises {
     assert(Tree.depth(sT) == 2)
     assert(Tree.depth(iT) == 3)
 
+    // Ex 3.28.  Write map.
+    assert( Tree.map(sT)((s: String) => s + s.toUpperCase) match {
+      case Branch(Branch(Leaf(a), Leaf(b)), Branch(Leaf(c), Leaf(d))) =>
+        a == "aA" && b == "bB" && c == "cC" && d == "dD"
+      case _ => false
+    })
 
+    // Ex. 3.29. Write fold, and use it in size, maximum, depth, and map.
+    assert(Tree.fold(iT)(i => i)(_ + _) == 15)
+    assert(Tree.fold(sT)(a => a)(_ + _) == "abcd")
 
+    assert(Tree.foldSize(sT) == 7)
+    assert(Tree.foldSize(iT) == 9)
+
+    assert(Tree.foldMaximum(iT) == 5)
+
+    assert( Tree.foldMap(sT)((s: String) => s + s.toUpperCase) match {
+      case Branch(Branch(Leaf(a), Leaf(b)), Branch(Leaf(c), Leaf(d))) =>
+        a == "aA" && b == "bB" && c == "cC" && d == "dD"
+      case _ => false
+    })
   }
 
 }
