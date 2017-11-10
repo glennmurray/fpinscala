3d24764ae8b446aad0ad9a1b2113429eb67d9a60
Glenn Murray <glenn.murray@gmail.com>
2017-11-09 17:13:23 -0700

    Finished Options exercises through 4.2

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -15,13 +15,31 @@ sealed trait Option[+A] {
   // The "default: => B" indicates that the argument is
   // of type B, but won’t be evaluated until it’s needed
   // by the function.
-  def getOrElse[B>:A](default: => B): B = ???
+  //def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = this match {
+    case Some(a) => a
+    case _       => default
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  //def flatMap[B](f: A => Option[B]): Option[B] = ???
+  // def flatMap[B](f: A => Option[B]): Option[B] = this match {
+  //   case Some(a) => f(a)
+  //   case _       => None
+  // }
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  //def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = ob
 
-  def filter(f: A => Boolean): Option[A] = ???
+  // Convert Some to None if the value does not satisfy f.
+  //def filter(f: A => Boolean): Option[A] = ???
+  // def filter(f: A => Boolean): Option[A] = this match {
+  //   case Some(a) if f(a) => Some(a)
+  //   case _               => None
+  // }
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -52,10 +70,15 @@ object Option {
     catch { case e: Exception => 43 }
   }
 
+  // Exercise 4.2 Implement the variance function in terms of flatMap.
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  //def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2)) ))
+  }
+
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
@@ -67,26 +90,35 @@ object Option {
 
 object OptionExercises {
   def main(args: Array[String]): Unit = {
-    val someInt = Some(42)
 
     // Ex. 4.1 Implement all of the preceding functions on Option
 
     // Implement def map[B](f: A => B): Option[B]
     assert( Some(42).map( x => x + 1) == Some(43) )
 
-    // Implement  def getOrElse[B>:A](default: => B): B = ???
+    // Implement  def getOrElse[B>:A](default: => B): B
+    assert( Some(42).getOrElse("else") == 42 )
+    assert( None.getOrElse("else") == "else" )
 
-    // Implement  def flatMap[B](f: A => Option[B]): Option[B] = ???
+    // Implement  def flatMap[B](f: A => Option[B]): Option[B]
+    assert( Some(42).flatMap( x => Some(x + 1)) == Some(43) )
+    assert( Some(42).flatMap( x => None) == None )
 
-    // Implement  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
 
-    // Implement  def filter(f: A => Boolean): Option[A] = ???
+    // Implement  def orElse[B>:A](ob: => Option[B]): Option[B]
+    val x = None.orElse(Some(41)).orElse(None).orElse(Some(42))
+    //println(s"*** x = $x")
+    assert( x == Some(42) )
 
-    // // Ex. 3.2, implement tail.
-    // assert(List.tail(l) match {
-    //   case Cons(2, Cons(3, Nil)) => true
-    //   case _ => false
-    // })
+    // Implement  def filter(f: A => Boolean): Option[A]
+    // Convert Some to None if the value does not satisfy f.
+    def even(i: Int): Boolean = i % 2 == 0
+    assert( Some(42).filter(even) == Some(42) )
+    assert( Some(41).filter(even) == None ) 
+    assert( None.filter(even) == None ) 
 
+    // Exercise 4.2 Implement the variance function in terms of flatMap.
+    val xs: Seq[Double] = Seq(1, 2, 3, 4)
+    assert( fpinscala.errorhandling.Option.variance(xs) == Some(1.25) )
   }
 }
