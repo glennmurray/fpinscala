4025f5239b68afceb4e993e39998c947afd63ea6
Glenn Murray <glenn.murray@gmail.com>
2018-03-27 21:34:19 -0600

    Comments in Stream.scala.

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,24 +3,43 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    // The arrow `=>` in front of the argument type `B` means that the
+    // function `f` takes its second argument by name and may choose not to
+    // evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      // If `f` doesn't evaluate its second argument, the recursion never
+      // occurs.
       case _ => z
     }
 
   def exists(p: A => Boolean): Boolean = 
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+    foldRight(false)((a, b) => p(a) || b)
+  // Here `b` is the unevaluated recursive step that folds the tail of the
+  // stream. If `p(a)` returns `true`, `b` will never be evaluated and the
+  // computation terminates early.
 
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
+
+  // Exercise 5.1 Write a function to convert a Stream to a List, which will
+  // force its evaluation and let you look at it in the REPL.
+  // This done as toListRecursive and tail recursive in the answers.
+  def toList: List[A] = ???
+
+  // Exercise 5.2 Write the function take(n) for returning the first n
+  // elements of a Stream, and drop(n) for skipping the first n elements of a
+  // Stream.
   def take(n: Int): Stream[A] = ???
 
   def drop(n: Int): Stream[A] = ???
 
+  // Exercise 5.3 Write the function takeWhile for returning all starting
+  // elements of a Stream that match the given predicate.
   def takeWhile(p: A => Boolean): Stream[A] = ???
 
   def forAll(p: A => Boolean): Boolean = ???
