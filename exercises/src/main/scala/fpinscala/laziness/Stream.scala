1d57f0fc7de962ee7a6dfa086fda5f03f228187b
Glenn Murray <glenn.murray@gmail.com>
2018-09-17 22:40:21 -0600

    Stream Exercise through 5.5.

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -39,6 +39,7 @@ trait Stream[+A] {
   // elements of a Stream, and drop(n) for skipping the first n elements of a
   // Stream.
   //def take(n: Int): Stream[A] = ???
+  //@annotation.tailrec
   def take(n: Int): Stream[A] = this match {
     case Empty                => Empty
     case Cons(h, t) if n < 1  => this
@@ -47,14 +48,35 @@ trait Stream[+A] {
     case Cons(h, t)           => cons( h(), t().take(n - 1) )
 
   }
-
-  def drop(n: Int): Stream[A] = ???
+  //def drop(n: Int): Stream[A] = ???
+  def drop(n: Int): Stream[A] = this match {
+    case Empty                => Empty
+    case Cons(h, t) if n == 0 => this
+    case Cons(h, t)           => t().drop(n - 1)
+  }
 
   // Exercise 5.3 Write the function takeWhile for returning all starting
   // elements of a Stream that match the given predicate.
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  //def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _                    => Empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = ???
+  // Exercise 5.4  Implement forAll, which checks that all elements in the
+  // Stream match a given predicate. Your implementation should terminate the
+  // traversal as soon as it encounters a nonmatching value.
+  def forAll(p: A => Boolean): Boolean = 
+    foldRight(true)((a, b) => p(a) && b)
+
+  //Exercise 5.5  Use foldRight to implement takeWhile.
+  def takeWhileFr(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])( (h, t) => if (p(h)) cons(h, t) else Empty )
+
+  def takeWhileFrAnswer(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h,t) =>
+      if (f(h)) cons(h,t)
+      else      empty)
 
   def headOption: Option[A] = ???
 
@@ -66,7 +88,12 @@ trait Stream[+A] {
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
+
+/**  The companion object.  */
 object Stream {
+  /**
+    * The "smart" constructor.
+    */
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
@@ -104,10 +131,34 @@ object StreamExercises {
       assert( s52.take(0).toList  == s52.toList )
       assert( s52.take(-2).toList == s52.toList )
       assert( s52.take(1).toList  == s52.toList.take(1) )
+      assert( s52.take(2).toList  == s52.toList.take(2) )
       assert( s52.take(9).toList  == s52.toList )
       assert( s52.take(s52.toList.length).toList  == s52.toList )
+      assert( Empty.drop(1) == Empty )
+      assert( s52.drop(0).toList == s52.toList )
+      assert( s52.drop(1).toList == List(2, 3, 4) )
+      assert( s52.drop(3).toList == List(4) )
+      assert( s52.drop(4).toList == List.empty[Int])
+      assert( s52.drop(5).toList == List.empty[Int])
 
+      // Exercise 5.3
+      def pred53(i: Int): Boolean = (i != 3)
+      assert( Empty.takeWhile(pred53).toList == List.empty[Int] )
+      assert( s52.takeWhile(pred53).toList == List(1, 2) )
+      val s53 = Stream(0, 2, 4, 6, 8)
+      assert( s53.takeWhile(pred53).toList == s53.toList )
 
+      // Exercise 5.4
+      def pred54(i: Int): Boolean = (i != 4)
+      assert( Empty.forAll(pred54) == true )
+      assert( s52.forAll(pred54) == false )
+      val s54 = Stream(1, 3, 5, 7)
+      assert( s54.forAll(pred54) == true )
+
+      // // Exercise 5.5
+      assert( Empty.takeWhileFr(pred53).toList == List.empty[Int] )
+      assert( s52.takeWhileFr(pred53).toList == List(1, 2) )
+      assert( s53.takeWhileFr(pred53).toList == s53.toList )
 
     }
 }
